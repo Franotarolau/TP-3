e3f54edb90f5bd608f62939c878fc083cc670dd3
Caleb Cordoba <calebcordoba21@gmail.com>
2026-06-23 14:56:48 -0600

    Avance de la 2 parte
    Aqui hice para poder modificar lo grande del estacionamiento y también si hay carros eléctricos  y también tiempo de gracia, y cobro por hora todavía falta lo demas, esto es en la parte de "Configuracion"

--- Funciones.py
+++ Funciones.py
@@ -3,6 +3,12 @@ from tkinter import *
 ventanaPrincipal = Tk()
 ventanaPrincipal.title("Sistema de Parqueo")
 ventanaPrincipal.geometry("400x400")
+#2
+#Configuracion
+tamanoEstacionamiento=0
+tiempoGracia=0
+montoHora=0
+tieneElectrico=False
 
 def ventanaPrincipalObtenerVehiculos():
     ventanaSecundaria = Toplevel()
@@ -19,6 +25,50 @@ def ventanaPrincipalReportes():
 def ventanaPrincipalConfiguracion():
     ventanaSecundaria = Toplevel()
     ventanaSecundaria.title("Configuración")
+        #2
+    ventanaSecundaria.geometry("350x300")
+    Label(
+        ventanaSecundaria,
+        text="Tamaño del estacionamiento:"
+    ).pack(pady=5)
+    entradaTamano=Entry(ventanaSecundaria)
+    entradaTamano.pack()
+    Label(
+        ventanaSecundaria,
+        text="Tiempo de gracia (minutos):"
+    ).pack(pady=5)
+    entradaTiempoGracia=Entry(ventanaSecundaria)
+    entradaTiempoGracia.pack()
+    Label(
+        ventanaSecundaria,
+        text="Monto por hora:").pack(pady=5)
+    entradaMontoHora=Entry(ventanaSecundaria)
+    entradaMontoHora.pack()
+    electrico=IntVar()
+    Checkbutton(
+        ventanaSecundaria,
+        text="Posee espacio para vehículos eléctricos",
+        variable=electrico
+    ).pack(pady=10)
+    def guardarConfiguracion():
+        global tamanoEstacionamiento
+        global tiempoGracia
+        global montoHora
+        global tieneElectrico
+        try:
+            tamanoEstacionamiento=int(entradaTamano.get())
+            tiempoGracia=int(entradaTiempoGracia.get())
+            montoHora=int(entradaMontoHora.get())
+            tieneElectrico=bool(electrico.get())
+            messagebox.showinfo("Configuración","Configuración guardada correctamente.")
+            ventanaSecundaria.destroy()
+        except ValueError:
+            messagebox.showerror("Error","Ingrese únicamente números.")
+    Button(
+        ventanaSecundaria,
+        text="Guardar Configuración",
+        command=guardarConfiguracion
+    ).pack(pady=15)
 
 def ventanaPrincipalAcercaDe():
     ventanaSecundaria = Toplevel()
